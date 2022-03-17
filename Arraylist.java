5fcf5efe7907e65ba98f5d6f7c1193e724651027
KAZA-CHAMANDESWARI <91886317+KAZA-CHAMANDESWARI@users.noreply.github.com>
2022-03-17 18:32:54 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Arraylist
+{
+    public static void main (String args[])
+    {
+        ArrayList<String> obj =new ArrayList<String>();
+        obj.add("97");
+        obj.add("87");
+        obj.add("86");
+        obj.add("56");
+        obj.add("78");
+        obj.add("94");
+    
+        System.out.println(obj);
+    }
+}
