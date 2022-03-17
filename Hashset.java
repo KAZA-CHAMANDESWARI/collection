73ce0c6c073f5f7a701c37992f88d99b663d9fc4
KAZA-CHAMANDESWARI <91886317+KAZA-CHAMANDESWARI@users.noreply.github.com>
2022-03-17 18:09:53 +0530

    Add files via upload

---
+++ Hashset.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Hashset
+{
+    public static void main (String args[])
+    {
+        HashSet<String> obj =new HashSet<String>();
+        obj.add("97");
+        obj.add("87");
+        obj.add("86");
+        obj.add("56");
+        obj.add("78");
+        obj.add("94");
+    
+        System.out.println(obj);
+    }
+}
